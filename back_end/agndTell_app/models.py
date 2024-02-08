b9dbc0ae6b3ff5b6db4900fe1f45d19e9de6363f
melquetrindade <melquetrindade654@gmail.com>
2024-02-08 11:41:09 -0300

    refazendo o front-end, adptando a vários tamanhos de tela

--- back_end/agndTell_app/models.py
+++ back_end/agndTell_app/models.py
@@ -13,7 +13,7 @@ class Clientes(models.Model):
 
 class Contatos(models.Model):
     nome = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.nome
@@ -31,7 +31,7 @@ class EnderecoContato(models.Model):
 
 class Telefones(models.Model):
     owner = models.ForeignKey(Contatos, on_delete=models.CASCADE)
-    telefone = models.CharField(max_length=25)
+    telefone = models.CharField(max_length=25, unique=True)
 
     def __str__(self):
         return self.telefone
