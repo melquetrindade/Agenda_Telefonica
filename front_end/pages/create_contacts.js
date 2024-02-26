1619a031897afb5d78a49b4a26e21c6a9180b43d
melquetrindade <melquetrindade654@gmail.com>
2024-02-26 14:17:53 -0300

    concluíndo a pag de criar contatos e media queries também

--- front_end/pages/create_contacts.js
+++ front_end/pages/create_contacts.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-import styles from '../styles/create_contacts.module.css'
-import {notification, message} from 'antd'
 import { useRouter } from "next/router";
+import styles from '../styles/create_contacts.module.css'
+import {notification} from 'antd'
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container'
@@ -11,216 +11,294 @@ import Row from 'react-bootstrap/Row';
 import Modal from 'react-bootstrap/Modal';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
-export default function DeleteContacts(){
+export default function EditContacts(){
 
-  const router = useRouter()
+    const router = useRouter()
 
-  const [inputValue, setInput] = useState({
-    nome: '',
-    email: '',
-  })
-
-  const [api, contextHolder] = notification.useNotification();
-  const openNotification = ({placement, title, descricao}) => {
-    api.info({
-        message: `${title}`,
-        description: `${descricao}`,
-        placement,
-    });
-  }
-
-  const [messageApi, contextHolder2] = message.useMessage();
-  const key = 'updatable';
-
-  const openMessage = () => {
-      messageApi.open({
-      key,
-      type: 'loading',
-      content: 'Loading...',
-      });
-      setTimeout(() => {
-          messageApi.open({
-              key,
-              type: 'success',
-              content: 'Loaded!',
-              duration: 2,
-          });
-      }, 1000);
-  };
-
-  const apiUrl = 'http://127.0.0.1:8000/clientes/'
-
-  const formatData = () => {
-
-    const registerData = {
-      nome: document.getElementById('formGridName').value,
-      email: document.getElementById('formGridEmail').value,
-    };
-
-    if(!registerData.nome){
-      openNotification({placement: 'topRight', title: 'ERRO', descricao: 'Preencha os Campos Obrigatórios'})
-    }
-    else if(!registerData.endereco){
-      openNotification({placement: 'topRight', title: 'ERRO', descricao: 'Preencha os Campos Obrigatórios'})
-    }
-    else if(!registerData.idade){
-      openNotification({placement: 'topRight', title: 'ERRO', descricao: 'Preencha os Campos Obrigatórios'})
-    }
-    else{
-      console.log('chama função de cadastrar')
-      cadastrarDados({objData: registerData})
-    }
-    //console.log(document.getElementById('formGridName').value)
-    //console.log(document.getElementById('formGridAge').value)
-    //console.log(document.getElementById('formGridAddress').value)
-  }
-
-  const cadastrarDados = async ({objData}) => {
-    openMessage()
-    fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(objData),
+    const [objContato, setContato] = useState({
+        nome: '',
+        email: ''
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Erro na requisição: ${response.status}`);
+    const [idContact, setId] = useState(undefined)
+    const [has_replay, setReplay] = useState('load')
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [api, contextHolder] = notification.useNotification();
+    const openNotification = ({placement, title, descricao}) => {
+        api.info({
+            message: `${title}`,
+            description: `${descricao}`,
+            placement,
+        });
+    }
+    
+    const criarContato = async ({objData}) => {
+        fetch(`http://127.0.0.1:8000/contatos/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(objData),
+        })
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            setId(data.id)
+            handleShow()
+            
+        })
+        .catch(error => {
+            console.error('Erro durante a requisição POST:', error);
+        });
+    }
+
+    console.log(`has_replay: ${has_replay}`)
+
+    const checkEmail = async ({email}) => {
+        fetch(`http://127.0.0.1:8000/contatos/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        })
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            var filterData = data.filter(item => item.email == email)
+            if(filterData.length != 0){
+                setReplay('verdadeiro')
+            }
+            else{
+                setReplay('falso')
+            }
+        })
+        .catch(error => {
+            console.error('Erro durante a requisição POST:', error);
+        });
+    }
+
+    if(has_replay == 'falso'){
+        setReplay('load')
+        const dataContato = {
+            nome: document.getElementById('formGridName').value,
+            email: document.getElementById('formGridEmail').value,
+        };
+
+        var ok = true
+        try{
+            Object.keys(dataContato).forEach(key => {
+                if(!dataContato[key]){
+                    ok = false
+                    openNotification({placement: 'topRight', title: 'ERRO', descricao: 'Preencha os Campos Obrigatórios'})
+                    throw new Error('StopIteration');
+                    
+                }
+            });
+        } catch(error){
+            if (error.message !== 'StopIteration') {
+                console.log('campo em branco')
+            }
         }
-        return response.json();
-      })
-      .then(data => {
-        openNotification({placement: 'topRight', title: 'Contato Cadastrado', descricao: 'O Contado foi Cadastrado com Sucesso!'})
-        setTimeout(function () {
-          router.back()
-        }, 2000);
-      })
-      .catch(error => {
-        console.error('Erro durante a requisição POST:', error);
-      });
-  }
 
-  const handleChangeName = (e) => {
-    const inputText = e.target.value
+        if(ok){
+            criarContato({objData: dataContato})
+        }
+    }
+    if(has_replay == 'verdadeiro'){
+        setReplay('load')
+        openNotification({placement: 'topRight', title: 'ERRO', descricao: 'Este E-mail já está sendo usado!'})
+    }
 
-    if (/^[a-zA-Z 0-9']+$/.test(inputText) || inputText === '') {
-        setInput({
-          nome: inputText,
-          endereco: inputValue.endereco,
-          idade: inputValue.idade
+    const formatData = () => {
+        checkEmail({email: document.getElementById('formGridEmail').value})
+    }
+
+    const handleChangeName = (e) => {
+        const inputText = e.target.value
+
+        if (/^[a-zA-Z 0-9']+$/.test(inputText) || inputText === '') {
+            setContato({
+                nome: inputText,
+                email: objContato.email,
+                status: objContato.status
+            })
+        }
+    }
+
+    const handleChangeEmail = (e) => {
+        const inputText = e.target.value
+
+        if (/^[a-zA-Z 0-9 @ . ']+$/.test(inputText) || inputText === '') {
+            setContato({
+                nome: objContato.nome,
+                email: inputText,
+                status: objContato.status
+            })
+        }
+    }
+
+    const cancelOperation = () => {
+        router.push({
+            pathname: './contacts'
         })
     }
-  }
 
-  const handleChangeAddress = (e) => {
-    const inputText = e.target.value
-
-    if (/^[a-zA-Z 0-9']+$/.test(inputText) || inputText === '') {
-      setInput({
-        nome: inputValue.nome,
-        endereco: inputText,
-        idade: inputValue.idade
-      })
+    const navEndereco = () => {
+        router.push({
+            pathname: './edit_endereco',
+            query: {id: idContact}
+        })
     }
-  }
 
-  const handleChangeAge = (e) => {
-    const inputText = e.target.value
+    return(
+        <main className={styles.main}>
+            {
+                objContato.status == 'load'
+                ?
+                    <Load/>
+                :
+                objContato.status == 'erro'
+                ?
+                    <Error/>
+                :
+                <Forms
+                    context01={contextHolder}
+                    changeName={handleChangeName}
+                    changeEmail={handleChangeEmail}
+                    nome={objContato.nome}
+                    email={objContato.email}
+                    funcNext={navEndereco}
+                    funcCancel={cancelOperation}
+                    show={show}
+                    funcHandleClose={handleClose}
+                    funcFormatData={formatData}
+                />
+            }
+        </main>
+    )
+}
 
-    if (/^[0-9']+$/.test(inputText) || inputText === '') {
-      setInput({
-        nome: inputValue.nome,
-        endereco: inputValue.endereco,
-        idade: inputText
-      })
-    }
-  }
+function Forms({
+    context01,
+    changeName, 
+    nome, 
+    changeEmail, 
+    email, 
+    funcNext,
+    funcCancel,
+    show,
+    funcHandleClose,
+    funcFormatData
+    }){
 
-  return(
-      <main className={styles.main}>
-        {contextHolder}
-        {contextHolder2}
-        <div className={styles.contProgress}>
-          <ProgressBar className={styles.progress} now={20}/>
-          <div className={styles.spanEmail}><span class="material-symbols-outlined">contact_mail</span></div>
-          <div className={styles.spanHome}><span class="material-symbols-outlined">home</span></div>
-          <div className={styles.spanCall}><span class="material-symbols-outlined">call</span></div>
-        </div>
-        <h1 style={{fontWeight: '300', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>Contatos</h1>
-        <hr></hr>
-        <Container>
-            <Row className={styles.rowTop}>
-                <Col className={styles.colOne}>
-                    <Form className={styles.formContato}>
-                        <Form.Group controlId="formGridName">
-                        <Form.Label>Nome</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Fulano de Tal" 
-                            required 
-                            minLength="1" 
-                            maxlength="250"
-                            onChange={changeName}
-                            value={nome}
-                        />
-                        </Form.Group>
+    return(
+        <div className={styles.body}>
+            {context01}
+            <div className={styles.contProgress}>
+                <ProgressBar className={styles.progress} now={20}/>
+                <div className={styles.spanEmail}><span class="material-symbols-outlined">contact_mail</span></div>
+                <div className={styles.spanHome}><span class="material-symbols-outlined">home</span></div>
+                <div className={styles.spanCall}><span class="material-symbols-outlined">call</span></div>
+            </div>
+            <h1 style={{fontWeight: '300', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>Contatos</h1>
+            <hr></hr>
+            <Container>
+              <Row className={styles.rowTop}>
+                  <Col className={styles.colOne}>
+                      <Form className={styles.formContato}>
+                          <Form.Group controlId="formGridName">
+                          <Form.Label>Nome</Form.Label>
+                          <Form.Control 
+                              type="text" 
+                              placeholder="Fulano de Tal" 
+                              required 
+                              minLength="1" 
+                              maxlength="250"
+                              onChange={changeName}
+                              value={nome}
+                          />
+                          </Form.Group>
+      
+                          <Form.Group controlId="formGridEmail">
+                          <Form.Label>E-mail</Form.Label>
+                          <Form.Control 
+                              type="text" 
+                              placeholder="fulado@gmail.com" 
+                              required 
+                              minLength="1" 
+                              maxlength="250"
+                              onChange={changeEmail}
+                              value={email}
+                          />
+                          </Form.Group>
+                      </Form>
+                  </Col>
+              </Row>
+            </Container>
+
+            <div className={styles.contButtons}>
+                <Button variant="success" size="sm" onClick={funcFormatData}>
+                    Criar Contato<span class="material-symbols-outlined">check</span>
+                </Button>
     
-                        <Form.Group controlId="formGridEmail">
-                        <Form.Label>E-mail</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="fulado@gmail.com" 
-                            required 
-                            minLength="1" 
-                            maxlength="250"
-                            onChange={changeEmail}
-                            value={email}
-                        />
-                        </Form.Group>
-                    </Form>
-                </Col>
+                <Button variant="danger" size="sm" onClick={funcCancel}>
+                    Cancelar Operação<span class="material-symbols-outlined">cancel</span>
+                </Button>
 
-            </Row>
+                <Modal
+                    show={show}
+                    onHide={funcHandleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Contato cadastrado com sucesso!</Modal.Title>
+                    </Modal.Header>
 
-        </Container>
-        <div className={styles.contButtons}>
-            <Button variant="success" size="sm" onClick={funcFormatData}>
-                Salvar Alterações<span class="material-symbols-outlined">check</span>
-            </Button>
+                    <Modal.Body>
+                        Para onde deseja ser redirecionado?
+                    </Modal.Body>
 
-            <Button variant="primary" size="sm" onClick={funcNext}>
-                Editar Endereço<span class="material-symbols-outlined">navigate_next</span>
-            </Button>
-
-            <Button variant="danger" size="sm" onClick={funcCancel}>
-                Cancelar Alterações<span class="material-symbols-outlined">cancel</span>
-            </Button>
-
-            <Modal
-                show={show}
-                onHide={funcHandleClose}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Alterações realizadas com sucesso!</Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    Para onde deseja ser redirecionado?
-                </Modal.Body>
-
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={funcCancel}>
-                        Página Inicial
-                    </Button>
-                    <Button variant="primary" onClick={funcNext}>
-                        Edição de Endereços
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={funcCancel}>
+                            Página Inicial
+                        </Button>
+                        <Button variant="primary" onClick={funcNext}>
+                            Edição de Endereços
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         </div>
-      </main>
-  )
+    )
+}
+
+function Load(){
+    return(
+        <div className={styles.fade}>
+            <div class="spinner-border text-info" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
+}
+
+function Error(){
+    return(
+        <h1>Error</h1>
+    )
 }
